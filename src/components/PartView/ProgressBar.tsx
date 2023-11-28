2c12066c3892c36ba7c5380cb47932c1eb81af19
Glenn <glenn.adriaensens@thomasmore.be>
2023-11-28 15:29:55 +0100

    refactor[ProgressBar]: fixed progressbar styling

--- src/components/PartView/ProgressBar.tsx
+++ src/components/PartView/ProgressBar.tsx
@@ -5,17 +5,14 @@ type ProgressBarProps = {
 export const Progressbar = ({ value, max }: ProgressBarProps) => {
 	const percentile = (value * 100) / max;
 	return (
-		<>
-			<div className="w-full bg-neutral-600 rounded-full relative h-5">
-				<div
-					className="bg-primary p-0.5 text-center text-xs font-medium leading-none text-primary-100 rounded-full h-full"
-					style={{ width: `${percentile}%` }}
-				>
-					<p className="absolute text-center align-middle m-0 left-0 right-0 top-1/2 -translate-y-1/2">
-						{value} / {max}
-					</p>
-				</div>
-			</div>
-		</>
+		<div className="w-full bg-neutral-600 rounded-full overflow-hidden relative h-5">
+			<div
+				className="bg-primary h-full"
+				style={{ width: `${percentile}%` }}
+			/>
+			<p className="absolute text-center text-white m-0 left-0 right-0 top-1/2 -translate-y-1/2 text-sm">
+				{value} / {max}
+			</p>
+		</div>
 	);
 };
