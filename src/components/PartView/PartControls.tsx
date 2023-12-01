d23ebbe43e88e799a579409128bcdc9bdba95994
Glenn <glenn.adriaensens@thomasmore.be>
2023-12-01 23:59:05 +0100

    feat: started modal form for putting reel in storage

--- src/components/PartView/PartControls.tsx
+++ src/components/PartView/PartControls.tsx
@@ -56,8 +56,80 @@ export const PartControls = () => {
 							</button>
 						</div>
 						Controls
-						<button className="btn">Fetch</button>
-						<button className="btn">Put</button>
+						<button className="btn">Get reel</button>
+						<button
+							className="btn"
+							onClick={() =>
+								(
+									document.getElementById("putReelModal")! as HTMLDialogElement
+								).showModal()
+							}
+						>
+							Put reel
+						</button>
+						<dialog
+							id="putReelModal"
+							className="modal"
+						>
+							<div className="modal-box">
+								<h3 className="font-bold text-lg">Put reel</h3>
+								<form
+									action=""
+									method="post"
+								>
+									<label className="form-control w-full max-w-xs">
+										<div className="label">
+											<span className="label-text">Part *</span>
+										</div>
+										<select className="select select-bordered">
+											<option
+												disabled
+												selected
+											>
+												Select part...
+											</option>
+											<option>Star Wars</option>
+											<option>Harry Potter</option>
+											<option>Lord of the Rings</option>
+											<option>Planet of the Apes</option>
+											<option>Star Trek</option>
+										</select>
+									</label>
+									<div className="flex gap-2 w-full">
+										<label className="form-control w-1/2 max-w-xs">
+											<div className="label">
+												<span className="label-text">Quantity</span>
+											</div>
+											<input
+												type="number"
+												placeholder="0"
+												className="input input-bordered w-full max-w-xs"
+											/>
+										</label>
+										<label className="form-control w-1/2 max-w-xs">
+											<div className="label">
+												<span className="label-text">MPN</span>
+											</div>
+											<input
+												type="text"
+												placeholder="insert MPN"
+												className="input input-bordered w-full max-w-xs"
+											/>
+										</label>
+									</div>
+								</form>
+								<form method="dialog">
+									<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+										✕
+									</button>
+								</form>
+								<div className="modal-action">
+									<form method="dialog">
+										<button className="btn">Close</button>
+									</form>
+								</div>
+							</div>
+						</dialog>
 					</div>
 				</Card.CardBody>
 			</Card.CardContainer>
