78ff576eff766caea1b1db35504ceb90e1cea96c
Glenn <glenn.adriaensens@thomasmore.be>
2024-03-15 09:57:03 +0100

    Started excel import

--- src/modules/Storage/Storage.tsx
+++ src/modules/Storage/Storage.tsx
@@ -3,6 +3,340 @@ import { Button } from "../../components/Button";
 import Card from "../../components/Card/Card";
 import { PutReelModal } from "../../components/Modals/PutReelModal";
 import { LocationList } from "../../components/Storage/LocationList";
+import { ReactSpreadsheetImport } from "react-spreadsheet-import";
+import { useState } from "react";
+
+const fields = [
+	{
+		label: "Part name",
+		key: "name",
+		fieldType: {
+			type: "input",
+		},
+		example: "R_10K_0603_10%_100mW",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Part name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Minimal stock",
+		key: "min_stock",
+		fieldType: {
+			type: "input",
+		},
+	},
+	{
+		label: "Description",
+		key: "description",
+		fieldType: {
+			type: "input",
+		},
+	},
+	{
+		label: "Package",
+		key: "package",
+		fieldType: {
+			type: "input",
+		},
+		example: "0603",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Package is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+	{
+		label: "Category name",
+		key: "cat_name",
+		fieldType: {
+			type: "input",
+		},
+		example: "resistor",
+		validations: [
+			{
+				rule: "required",
+				errorMessage: "Category name is required",
+				level: "error",
+			},
+		],
+	},
+];
 
 const getReel = (e: React.MouseEvent<HTMLElement>) => {
 	axios
@@ -13,7 +347,16 @@ const getReel = (e: React.MouseEvent<HTMLElement>) => {
 		});
 };
 
+const submitExcel = (data: any, file: any) => {
+	console.log(data);
+};
+
 export const Storage = () => {
+	const [isOpen, setIsOpen] = useState(true);
+	const onClose = () => {
+		setIsOpen(false);
+	};
+
 	return (
 		<>
 			<div className="flex flex-col w-full md:flex-shrink-0 md:flex-row justify-center">
@@ -66,6 +409,19 @@ export const Storage = () => {
 										<h1 className="text-white text-lg font-bold">Locations:</h1>
 										<div className="flex w-full gap-5">
 											<LocationList />
+										</div>
+									</div>
+								</div>
+								<div className="flex flex-col w-full md:flex-shrink-0 md:flex-row mt-2">
+									<div className="md:w-full md:shrink-0 gap-2">
+										<h1 className="text-white text-lg font-bold">Import</h1>
+										<div className="flex w-full gap-5">
+											<ReactSpreadsheetImport
+												isOpen={isOpen}
+												onClose={onClose}
+												onSubmit={submitExcel}
+												fields={fields}
+											/>
 										</div>
 									</div>
 								</div>
